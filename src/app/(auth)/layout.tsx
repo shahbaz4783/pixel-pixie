80a36da96786c9037f11e8ea7b1da786b4cda014
shahbaz4783 <shahbaz4783@gmail.com>
2024-02-17 16:52:40 +0530

    add clerk auth

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const layout = () => {
+  return (
+    <div>Auth layout</div>
+  )
+}
+
+export default layout
---
+++ src/constants/navLinks.ts
@@ -0,0 +1,44 @@
+import { MdDashboard } from 'react-icons/md';
+
+export const navLinks = [
+	{
+		label: 'Home',
+		path: '/',
+		icon: MdDashboard,
+	},
+	{
+		label: 'Image Restore',
+		path: '/transformations/add/restore',
+		icon: MdDashboard,
+	},
+	{
+		label: 'Generative Fill',
+		path: '/transformations/add/fill',
+		icon: MdDashboard,
+	},
+	{
+		label: 'Object Remove',
+		path: '/transformations/add/remove',
+		icon: MdDashboard,
+	},
+	{
+		label: 'Object Recolor',
+		path: '/transformations/add/recolor',
+		icon: MdDashboard,
+	},
+	{
+		label: 'Background Remove',
+		path: '/transformations/add/removeBackground',
+		icon: MdDashboard,
+	},
+	{
+		label: 'Profile',
+		path: '/profile',
+		icon: MdDashboard,
+	},
+	{
+		label: 'Buy Credits',
+		path: '/credits',
+		icon: MdDashboard,
+	},
+];
